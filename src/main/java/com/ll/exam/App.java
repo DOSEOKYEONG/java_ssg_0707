336ed79f42c278e36a6362ff95f01a3a37ff0b8d
DOSEOKYEONG <dho88dho@naver.com>
2022-07-07 15:12:50 +0900

    id로 리스트관리

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class App {
     public void run() {
+        int id;
         String author;
         String word;
         List<Word> wordList = new ArrayList<>();
@@ -23,13 +24,17 @@ public class App {
                 case "종료":
                     break outer;
                 case "등록":
+                    id = wordList.size()+1;
                     System.out.println("명언: ");
                     word = sc.nextLine().trim();
                     System.out.println("작가: ");
                     author = sc.nextLine().trim();
-                    word1 = new Word(author, word);
+                    word1 = new Word(id, author, word);
                     wordList.add(word1);
                     System.out.println(wordList.size() + "번 명언이 등록되었습니다.");
+                case "목록":
+                    for
+
             }
         }
 
